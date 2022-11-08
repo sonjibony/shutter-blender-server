54e437ba7b409ab2f08851219360471ce4fb46d2
sonjibony <sonjibony@gmail.com>
2022-11-08 14:59:53 +0600

    service api created

--- index.js
+++ index.js
@@ -13,11 +13,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3nhngvm.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run (){
+    try {
+const serviceCollection = client.db('shutterBlender').collection('services');
+
+//limited services api
+app.get('/limitedServices', async(req,res) =>{
+const query = {}
+const cursor = serviceCollection.find(query).limit(3);
+const services = await cursor.toArray();
+res.send(services);
+})
+
+//all service api
+app.get('/services', async(req,res) =>{
+const query = {}
+const cursor = serviceCollection.find(query);
+const services = await cursor.toArray();
+res.send(services);
+})
+    }
+
+finally{
+}
+
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req,res) => {
