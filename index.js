cd03db7dd4fdad23045e822b9bbbc20d86f241a3
sonjibony <sonjibony@gmail.com>
2022-11-08 15:37:22 +0600

    specific service api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,15 @@ const cursor = serviceCollection.find(query);
 const services = await cursor.toArray();
 res.send(services);
 })
+
+//specific service api
+app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const service = await serviceCollection.findOne(query);
+    res.send(service);
+})
+
     }
 
 finally{
