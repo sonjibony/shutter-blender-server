05e1688aa404a3d9222ea303d0fe8eeca63523e1
sonjibony <sonjibony@gmail.com>
2022-11-10 02:30:49 +0600

    jwt token created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
